9817a8eb4d27717402d88e5ec5cac6df38e7e286
YutoOkawa <is0344rf@ed.ritsumei.ac.jp>
2020-11-05 17:52:47 +0900

    fidoControlPoint
    CommandParameterの配列サイズの変更

--- src/ctap.cpp
+++ src/ctap.cpp
@@ -62,7 +62,7 @@ Request ControlPointCallbacks::parseRequest(uint8_t *req) {
     request.data.commandValue = (unsigned int)*req;
     req++;
     // Command Parameterを取得
-    request.data.commandParameter = new uint8_t[request.llen];
+    request.data.commandParameter = new uint8_t[request.llen-1];
     for (int i=request.hlen; i<request.llen-1; i++) {
         request.data.commandParameter[i] = *req;
         req++;
